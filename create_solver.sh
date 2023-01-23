575f5bb86314b2930eff8addf0358fb9ace3b2fc
Vilém Pantlík <eWillyo@users.noreply.github.com>
2023-01-23 04:57:59 +0100

    updated

--- create_solver.sh
+++ create_solver.sh
@@ -26,6 +26,7 @@ echo "Compiling.."
 g++ -Wall -DNDEBUG -I./glm config.h constants.h functions.h functions.cpp operations.h operations.cpp types.h types.cpp parser.h parser.cpp main.cpp -o math_solver &> /dev/null
 
 chmod +x math_solver
+ln -s ExpressionParser/math_solver $HOME/
 
 echo "Done!"
 sleep 2
